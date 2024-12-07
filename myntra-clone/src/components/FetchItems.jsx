9de2757f068a1b073cf3787e54a970bbce13e5e3
yashvardhan8787 <139737436+yashvardhan8787@users.noreply.github.com>
2024-12-07 11:00:00 +0530

    Update FetchItems.jsx
    backend - integrated

--- myntra-clone/src/components/FetchItems.jsx
+++ myntra-clone/src/components/FetchItems.jsx
@@ -13,7 +13,7 @@ const FetchItems = () => {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    fetch("http://localhost:8080/items",{signal})
+    fetch("https://my-myntra-backend.onrender.com/items",{signal})
     .then((res)=>res.json())
     .then(({items})=>{
       dispatch(fetchStatusActions.markFetchDone());
